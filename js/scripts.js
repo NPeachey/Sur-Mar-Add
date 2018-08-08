89236976bd36b5bff5fe8fa5b210d16844ab9314
catherine bradley and neil peachey <catherine@>
2018-08-08 16:39:50 -0700

    add response to result name in reciept

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function() {
   $("#survey").click(function() {
   var head = $("#head").val();
+  var flavor = $("input:radio[name=flavor]:checked").val();
+  var name = $("input#person1").val();
 
   $("#result").show();
   $("#beverage").hide();
   $("#page").hide();
 
+  $(".nameresult").append(name);
   $(".headresult").append(head);
+  $(".colorheadresult").append(flavor);
 
   event.preventDefault();
 
